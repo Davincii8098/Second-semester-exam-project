0800ec56444fb059506fb1f7cd36c3db48760c9c
Gilbert Adah <gilbertadah1@gmail.com>
2023-10-23 14:25:39 +0000

    First commit
    Changes to be committed:
    	deleted:    README.md
        new file:   my-app/.gitignore
    	new file:   my-app/README.md
    	new file:   my-app/package-lock.json
    	new file:   my-app/package.json
    	new file:   my-app/public/favicon.ico
    	new file:   my-app/public/index.html
    	new file:   my-app/public/logo192.png
    	new file:   my-app/public/logo512.png
    	new file:   my-app/public/manifest.json
    	new file:   my-app/public/robots.txt
    	new file:   my-app/src/App.css
    	new file:   my-app/src/App.js
    	new file:   my-app/src/App.test.js
    	new file:   my-app/src/Components/ErrorBoundary.jsx
    	new file:   my-app/src/Components/ErrorTest.jsx
    	new file:   my-app/src/Components/counter404.jsx
    	new file:   my-app/src/Hooks/UseCounter.js
    	new file:   my-app/src/index.css
    	new file:   my-app/src/index.js
    	new file:   my-app/src/logo.svg
    	new file:   my-app/src/reportWebVitals.js
    	new file:   my-app/src/requirement.txt
    	new file:   my-app/src/setupTests.js

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import useCounter from "./Hooks/UseCounter";
+
+function CounterApp() {
+  const { count, increment, decrement, reset, setValue } = useCounter(0);
+
+  return (
+    <div className = "container">
+    <div className = "counter-container">
+      <h1>Counter Box</h1>
+      <p>Count: {count}</p>
+
+      <div className = "counter">
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      </div>
+      <div className="actions">
+      <button onClick={reset}>Reset</button>
+      <input
+        type="number"
+        value={count}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      </div>
+
+    </div>
+    </div>
+  );
+}
+
+export default CounterApp;
